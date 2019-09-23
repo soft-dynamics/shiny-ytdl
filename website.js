cc58c6d78135e943d78ab52705960554b3684e80
Kitsune IDK <velocicraft2088@gmail.com>
2019-09-23 18:13:48 -0300

    New version, new resources

--- website.js
+++ website.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 try {
 	var package  = JSON.parse(fs.readFileSync('./package.json'));
 } catch (e) {
-	console.log("Don't found package.json, downloading the missing file.");
+	console.log("Don't found package.json, downloading the missing file...");
 	https.get('https://raw.githubusercontent.com/soft-dynamics/shiny-ytdl/master/package.json', (res) => {
 		var data = "";
 		res.on('data', (chunk) => {
@@ -16,15 +16,56 @@ try {
 		
 		res.on('end', () => {
 			fs.writeFileSync('./package.json', data);
-			console.log("File downloaded! Restarting the app");
+			console.log("File downloaded! Restarting the app...");
 			cmd.exec('node website.js');
-			process.exit(0);
 		});
 	}).on('error', (err) => {
-		console.log("Can't download package.json, please manually download in https://github.com/soft-dynamics/shiny-ytdl");
+		console.log("Can't download package.json, please manually download in https://github.com/soft-dynamics/shiny-ytdl.");
+		process.exit(1);
 	});
 }
 
 try {
-	var config = require('./config.json')
+	var config = require('./config.json');
+} catch (e) {
+	console.log("Don't found config.json, downloading the missing file...");
+	https.get('https://raw.githubusercontent.com/soft-dynamics/shiny-ytdl/master/config.json', (res) => {
+		var data = "";
+		res.on('data', (chunk) => {
+			data += chunk;
+		});
+		
+		res.on('end', () => {
+			fs.writeFileSync('./config.json', data);
+			console.log("File downloaded! Restarting the app...");
+			cmd.exec('node website.js');
+		});
+	}).on('error', (err) => {
+		console.log("Can't download config.json, please manually download in https://github.com/soft-dynamics/shiny-ytdl.");
+		process.exit(1);
+	});
+}
+
+try {
+	var express = require('express');
+	var contentDisposition = require('content-disposition');
+	var ejs = require('ejs');
+	var ytpl = require('ytpl');
+	var search = require('yt-search');
+	var ytdl = require('ytdl-core');
+	var ffmpeg = require('fluent-ffmpeg');
+} catch (e) {
+	console.log("Don't found dependencies, installing all needed dependencies...");
+	cmd.exec('npm install', (error, stdout, stderr) => {
+		if (error) {
+			console.log("A error has happen: "+error);
+			process.exit(1);
+		}
+		if (stderr) {
+			console.log("NPM returned a error: "+stderr);
+			process.exit(1);
+		}
+		console.log("Dependencies installed! Restarting the app...");
+		cmd.exec('node website.js');
+	});
 }
